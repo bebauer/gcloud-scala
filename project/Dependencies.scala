74ab27234660ab8b6213824621151b7b96096cf3
bebauer <benjamin.bauer@codecentric.de>
2018-04-12 09:20:39 +0200

    update testcontainers dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val GcloudPubSub        = "com.google.cloud" % "google-cloud-pubsub"   % "0.34.0-beta"
   lazy val Slf4jApi            = "org.slf4j"        % "slf4j-api"             % "1.7.25"
   lazy val LogbackClassic      = "ch.qos.logback"   % "logback-classic"       % "1.2.1"
-  lazy val TestcontainersScala = "com.dimafeng"     %% "testcontainers-scala" % "0.13.0"
+  lazy val TestcontainersScala = "com.dimafeng"     %% "testcontainers-scala" % "0.16.0"
   lazy val ScalaMeta           = "org.scalameta"    %% "scalameta"            % "3.3.0"
   lazy val Treehugger          = "com.eed3si9n"     %% "treehugger"           % "0.4.3"
 
